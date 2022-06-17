01223e574b4808f97cb497ae6d62ea552acde882
yu <yu@yuyao.com>
2022-06-17 19:07:04 +0800

    push commit

--- src/main/java/com/yao/git/GitTest.java
+++ src/main/java/com/yao/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello.git4");
         System.out.println("hello.master!");
         System.out.println("hot-fix test!");
+        System.out.println("push test!");
     }
 }
